546700a296896486ac5fd73d5a05c2c5818ad07a
qnxy <qnxy1997@gmail.com>
2024-07-05 16:21:53 +0800

    update port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const route = require('./routes');
 
 route(app)
 
-app.listen(8080, () => console.log('Server is running on port 8080'));
+app.listen(80, () => console.log('Server is running on port 8080'));
