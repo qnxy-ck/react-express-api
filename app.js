beed602e0e933011e32db3000a6e05f5f2986329
qnxy <qnxy1997@gmail.com>
2024-07-05 16:40:27 +0800

    增加启动时指定端口

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const route = require('./routes');
+const PORT = process.env.PORT || 9900;
+
 
 route(app)
 
-app.listen(9900, () => console.log('Server is running on port 8080'));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
